d2263ab2f693a76eca23d27e77aa0eb90a84a078
Renuka Panigrahi <panigrahirenuka599@gmail.com>
2020-08-21 15:35:31 -0400

    Eureka server created by using eurekaClient to client2 to client3 as response embedded

--- eureka-client-2/src/main/java/eureka/client2/EurekaClient2Application.java
+++ eureka-client-2/src/main/java/eureka/client2/EurekaClient2Application.java
@@ -1,10 +1,39 @@
 package eureka.client2;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class EurekaClient2Application {
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping("/")
+	public String home() throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("message", "Hello World 2");
+		jsonObject.put("message-3", new JSONObject(
+				restTemplate.exchange(
+						"http://localhost:8003/", HttpMethod.GET, null, String.class).getBody()));
+		return jsonObject.toString();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClient2Application.class, args);
