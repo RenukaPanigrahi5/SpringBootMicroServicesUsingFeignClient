21c25db53c384bf2fe52a37be84dc87f21690c7e
Renuka Panigrahi <panigrahirenuka599@gmail.com>
2020-08-23 16:35:20 -0400

    ribbon loadbalancer

--- eureka-client/src/main/java/eureka/client/EurekaClientApplication.java
+++ eureka-client/src/main/java/eureka/client/EurekaClientApplication.java
@@ -3,10 +3,12 @@ package eureka.client;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,14 @@ public class EurekaClientApplication {
 		jsonObject.put("message", "Hello World");
 		jsonObject.put("message-2", new JSONObject(myFeignClient.client2Response()));
 		return jsonObject.toString();
+	}
+	
+	@Value("${server.port}")
+	private String port;
+	
+	@GetMapping("/app")
+	public String getApp() {
+		return "the port is "+port;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
